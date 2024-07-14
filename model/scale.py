e59fd673ac78f037d9590e23b3bf70935e2f40d9
Hanlin Kong <43067268+MikeGoblin@users.noreply.github.com>
2024-07-14 19:46:50 +0800

    Update scale.py

--- model/scale.py
+++ model/scale.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def KnnDistance(pcloud, nb_neighbors):
+def KnnDistance(pcloud, nb_neighbors): # 用于计算点云中每个点与其k个最近邻点的平均距离
         # Size
-        nb_points = pcloud.shape[1]
+        nb_points = pcloud.shape[1] # 点的数量
         size_batch = pcloud.shape[0]
 
         # Distance between points
@@ -14,7 +14,7 @@ def KnnDistance(pcloud, nb_neighbors):
             pcloud, pcloud.transpose(1, 2)
         ) # [b,n,n]
 
-        distance_matrix[distance_matrix<0] = 0
+        distance_matrix[distance_matrix<0] = 0 # 由于计算误差可能会导致负数距离
         dis = torch.sqrt(torch.topk(distance_matrix, k=nb_neighbors, dim=2, largest=False, sorted=True).values) # # [b,n,k]
         # max_dis = dis[:,:,-1]
 
